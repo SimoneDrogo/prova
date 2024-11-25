055bd8492818a9f802f2cea42925305f5edc928f
Simone Drogo <s.drogo@campus.unimib.it>
2024-11-25 15:20:37 +0100

    ho inserito una classe che afferma che un double è maggiore dell'altro

--- src/main/java/org/junit/Assert.java
+++ src/main/java/org/junit/Assert.java
@@ -1031,4 +1031,9 @@ public class Assert {
     private static String buildPrefix(String message) {
         return message != null && message.length() != 0 ? message + ": " : "";
     }
+    
+    public static void assertGreaterThan (double x, double y) {
+        
+        assert x > y;
+    }
 }
